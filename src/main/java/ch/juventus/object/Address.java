bdaaa5a7c4cbb6cbf6aeda5154102b06f8dcabf9
test-user-2 <test-email-2>
2024-09-12 14:42:50 +0200

    Add object exercise

---
+++ src/main/java/ch/juventus/object/Address.java
@@ -0,0 +1,72 @@
+package ch.juventus.object;
+
+import java.util.Objects;
+
+public class Address {
+    private String street;
+    private int number;
+    private int zip;
+    private String city;
+
+    public Address(String street, int number, int zip, String city) {
+        this.street = street;
+        this.number = number;
+        this.zip = zip;
+        this.city = city;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+
+    public int getZip() {
+        return zip;
+    }
+
+    public void setZip(int zip) {
+        this.zip = zip;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    @Override
+    public String toString() {
+        return "Address{" +
+                "street='" + street + '\'' +
+                ", number=" + number +
+                ", zip=" + zip +
+                ", city='" + city + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Address address = (Address) o;
+        return number == address.number && zip == address.zip && Objects.equals(street, address.street) && Objects.equals(city, address.city);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(street, number, zip, city);
+    }
+}
---
+++ src/main/java/ch/juventus/object/Application.java
@@ -0,0 +1,26 @@
+package ch.juventus.object;
+
+public class Application {
+
+    public static void main(String[] args) {
+
+        Person linda = new Person(
+                "linda",
+                "krueger",
+                new Address("heimstrasse", 8, 8952, "dietikon"),
+                31,
+                false
+        );
+
+        Person lindaCopy = new Person(
+                "linda",
+                "krueger",
+                new Address("heimstrasse", 8, 8952, "dietikon"),
+                31,
+                false
+        );
+
+        System.out.println(linda.equals(lindaCopy));
+
+    }
+}
