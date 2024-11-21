e70d7a1d3a66468df406ef76bd579a4d4696a5ef
Linda Krueger <lkrueger17@gmail.com>
2024-11-21 15:26:55 +0100

    Add Mockito exercise

---
+++ src/test/java/ch/juventus/mocking/UserServiceExerciseTest.java
@@ -0,0 +1,18 @@
+package ch.juventus.mocking;
+
+class UserServiceExerciseTest {
+
+    // Todo: test getActiveUsers:
+    // 1. with empty users list
+    // 2. with list containing both active and inactive users
+
+    // Todo: test findActiveUser:
+    // 1. with name which doesn't exist in the users list
+    // 2. with name which exists in users list but is not active
+    // 3. with name which exists in users list and is active
+
+    // Todo: test getAdultUsers
+    // 1. with users list containing only users with age < 18
+    // 2. with users list containing only users with age > 18
+    // 3. with users list containing only users with age 18, > 18, < 18
+}
